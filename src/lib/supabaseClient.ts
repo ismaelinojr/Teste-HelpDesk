c99784382d6377ea6d494c61bcb40bc7eba76feb
Ismael <ismael@example.com>
2026-03-10 14:37:47 -0300

    Arquivo env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pirgsvvjhneswqxatkhw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpcmdzdnZqaG5lc3dxeGF0a2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNzE4NjAsImV4cCI6MjA4ODY0Nzg2MH0.riiyek_2bWNrQ0w80PhDX5R_bW5_8nnB1SZpqKVuQeU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
